343f29301fd3cb8968d10d5633ab65fb28b3bd6e
Thomas1664 <46387399+Thomas1664@users.noreply.github.com>
2022-09-02 22:29:28 +0000

    tes

--- spdx_license_matcher/difference.py
+++ spdx_license_matcher/difference.py
@@ -1,6 +1,9 @@
 import difflib
+from errno import ERANGE
 
 import jellyfish
+import sys
+import itertools
 
 
 def generate_diff(originalLicenseText, inputLicenseText):
@@ -18,8 +21,40 @@ def generate_diff(originalLicenseText, inputLicenseText):
         lines.append(line)
     return lines
 
+def _check_type(s):
+    if True and not isinstance(s, str):
+        raise TypeError('expected str or unicode, got %s' % type(s).__name__)
+    elif not True and not isinstance(s, unicode):
+        raise TypeError('expected unicode, got %s' % type(s).__name__)
 
-def get_similarity_percent(text1, text2):
+
+def x(s1, s2):
+    _check_type(s1)
+    _check_type(s2)
+
+    if s1 == s2:
+        return 0
+    rows = len(s1)+1
+    cols = len(s2)+1
+
+    if not s1:
+        return cols-1
+    if not s2:
+        return rows-1
+
+    prev = None
+    cur = range(cols)
+    for r in range(1, rows):
+        prev, cur = cur, [r] + [0]*(cols-1)
+        for c in range(1, cols):
+            deletion = prev[c] + 1
+            insertion = cur[c-1] + 1
+            edit = prev[c-1] + (0 if s1[r-1] == s2[c-1] else 1)
+            cur[c] = min(edit, deletion, insertion)
+
+    return cur[-1]
+
+def get_similarity_percent(text1: str, text2: str):
     """Levenshtein distance, a string metric for measuring the difference between two sequences, is used to calculate the similarity percentage between two license texts.
 
     Arguments:
@@ -29,7 +64,14 @@ def get_similarity_percent(text1, text2):
     Returns:
         float -- similarity percentage between the two given texts.
     """
-    levDis = float(jellyfish.levenshtein_distance(text1, text2))
+    t1 = type(text1)
+    t2 = type(text2)
+    s1 = text1
+    s2 = text2
+    res = x(s1, s2)
+    
+    #res: int = jellyfish.levenshtein_distance(str(text1), str(text2))
+    levDis = float(res)
     bigger = float(max(len(text1), len(text2)))
     similarityPercentage = round((bigger - levDis) / bigger * 100, 2)
     return similarityPercentage
